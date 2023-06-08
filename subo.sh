a3bf5c0cf4761c25b2f9aeff2bbfa39c62a19420
cristianurcuqui <cristianurcuqui@gmail.com>
2023-06-07 23:57:42 -0500

    Ultimo Commit

--- subo.sh
+++ subo.sh
@@ -2,9 +2,9 @@ git add .
 git commit -m "Ultimo Commit"
 git push
 
-set GOOS=linux 
-set GOARCH=amd64 
+export GOOS=linux 
+export GOARCH=amd64 
 
-go build main.go
+go build -o main
 rm -f main.zip
 zip main.zip main
